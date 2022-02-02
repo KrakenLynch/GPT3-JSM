8922bf5e6a5e3326b1410aaa7fea0088ad7f6073
KrakenLynch <dastageersayed99@gmail.com>
2022-02-02 14:21:02 +0530

    Brands Section Complete

--- src/containers/whatGPT3/WhatGPT3.js
+++ src/containers/whatGPT3/WhatGPT3.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import {Feature} from "../../components/index"
 import './whatGPT3_styles.css'
 
 const WhatGPT3 = () => {
     return (
-        <div>
-           WhatGPT3 
+        <div className="gpt3__whatgpt3 section_margin" id="wgpt3">
+           <div className="gpt3__whatgpt3-feature">
+               <Feature />
+           </div>
+           <div className="gpt3__whatgpt3-heading">
+               <h1 className="gradient-text">
+               The possibilities are beyond your imagination
+               </h1>
+               <p>Explore The Library</p>
+           </div>
         </div>
     )
 }
