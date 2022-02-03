d39bb9034e5d44249adc36b8f92318952d51c747
KrakenLynch <dastageersayed99@gmail.com>
2022-02-03 22:55:16 +0530

    Started Gpt3 container

--- src/containers/whatGPT3/WhatGPT3.js
+++ src/containers/whatGPT3/WhatGPT3.js
@@ -1,21 +1,35 @@
 import React from 'react'
-import {Feature} from "../../components/index"
+import { Feature } from '../../components/index'
 import './whatGPT3_styles.css'
 
 const WhatGPT3 = () => {
-    return (
-        <div className="gpt3__whatgpt3 section_margin" id="wgpt3">
-           <div className="gpt3__whatgpt3-feature">
-               <Feature />
-           </div>
-           <div className="gpt3__whatgpt3-heading">
-               <h1 className="gradient-text">
-               The possibilities are beyond your imagination
-               </h1>
-               <p>Explore The Library</p>
-           </div>
-        </div>
-    )
+  const title_text =
+    'We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.'
+  const chat_text =
+    'We so opinion friends me message as delight. Whole front do of plate heard oh ought.'
+  const knowledge_text =
+    'At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b'
+  const education_text =
+    'At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b'
+
+  return (
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature title="What is GPT-3" text={title_text} />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore the Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature title="Chatbots" text={chat_text} />
+        <Feature title="Knowledgebase" text={knowledge_text} />
+        <Feature title="Education" text={education_text} />
+      </div>
+    </div>
+  )
 }
 
 export default WhatGPT3
