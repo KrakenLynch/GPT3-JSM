ded4bf1a0b8818b1ccc2f9301a503b8cf835e96a
KrakenLynch <dastageersayed99@gmail.com>
2022-02-07 19:54:40 +0530

    possibility container done

--- src/containers/possibility/Possibility.js
+++ src/containers/possibility/Possibility.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import possibilityImage from '../../assets/possibility.png'
 import './possibility_styles.css'
 
 const possibility = () => {
-  return <div>Possibility</div>
+  const content =
+    'Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.'
+
+  return (
+    <div className="gpt3__possibility section__padding" id="possibility">
+      <div className="gpt3__possibility-image">
+        <img src={possibilityImage} alt="possibility" />
+      </div>
+      <div className="gpt3__possibility-content">
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className="gradient__text">
+          The possibilities are <br /> beyond your imagination
+        </h1>
+        <p>{content}</p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
+    </div>
+  )
 }
 
 export default possibility
